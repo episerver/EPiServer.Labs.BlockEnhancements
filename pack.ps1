5a666e8605bf12c91c280b9f5ce4f4d2197df5f3
Bartosz Sekuła <bartosz.sekula@episerver.com>
2019-06-25 10:34:23 +0200

    fix powershell parameters

--- pack.ps1
+++ pack.ps1
@@ -1,12 +1,15 @@
 $defaultVersion="1.0.0"
 $workingDirectory = Get-Location
+$zip = "$workingDirectory\packages\7-Zip.CommandLine.18.1.0\tools\7za.exe"
+$nuget = "$workingDirectory\build\tools\nuget.exe"
+
 function ZipCurrentModule
 {
     Param ([String]$moduleName)
     Robocopy.exe $defaultVersion\ $version\ /S
-    ((Get-Content -Path module.config -Raw) -Replace $defaultVersion, $version ) | Set-Content -Path module.config
-    "packages\7-Zip.CommandLine.18.1.0\tools\7za.exe a $moduleName.zip $version Views module.config"
-    git checkout module.config
+    ((Get-Content -Path module.config -Raw) -Replace $defaultVersion, $version ) | Set-Content -Path module.config    
+    Start-Process -NoNewWindow -Wait -FilePath $zip -ArgumentList "a", "$moduleName.zip", "$version", "module.config"
+    ((Get-Content -Path module.config -Raw) -Replace $version, $defaultVersion ) | Set-Content -Path module.config
     Remove-Item $version -Force -Recurse
 }
 
@@ -17,4 +20,4 @@ Write-Host "Creating nuget with $version version"
 Set-Location src\alloy\modules\_protected\episerver-labs-block-enhancements
 ZipCurrentModule -moduleName episerver-labs-block-enhancements
 Set-Location $workingDirectory
-build\tools\nuget.exe pack src\alloy\EPiServer.Labs.BlockEnhancements.nuspec -Version $version
+Start-Process -NoNewWindow -Wait -FilePath $nuget -ArgumentList "pack", "$workingDirectory\src\alloy\EPiServer.Labs.BlockEnhancements.nuspec", "-Version $version"

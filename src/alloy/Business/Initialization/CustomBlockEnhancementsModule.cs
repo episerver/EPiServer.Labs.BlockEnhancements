8aea515dc70a0fd6b2654eb697db47b7dd75aa57
Bartosz Sekuła <bartosz.sekula@episerver.com>
2019-06-27 08:46:15 +0200

    Example configuration module

---
+++ src/alloy/Business/Initialization/CustomBlockEnhancementsModule.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Labs.BlockEnhancements;
+using EPiServer.ServiceLocation;
+
+namespace AlloyTemplates.Business.Initialization
+{
+    [InitializableModule]
+    public class CustomBlockEnhancementsModule : IInitializableHttpModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            var options = ServiceLocator.Current.GetInstance<BlockEnhancementsOptions>();
+            options.InlineEditing = true;
+            options.PublishWithLocalContentItems = true;
+            options.InlinePublish = true;
+            options.StatusIndicator = true;
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            
+        }
+
+        public void InitializeHttpEvents(HttpApplication application)
+        {
+            
+        }
+    }
+}

0d6aeef3ea87f57974691d63a6c11bf00d602430
Bartosz Sekuła <bartosz.sekula@episerver.com>
2019-08-01 23:38:01 +0200

    Start-Release 0.2.0

--- version.cs
+++ version.cs
@@ -1,7 +1,8 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("episerver-labs")]
 [assembly: AssemblyProduct("block-enhancements")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.2.0")]
+[assembly: AssemblyInformationalVersion("0.2.0-developerbuild")]

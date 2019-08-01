a3832b080a17b910128a08dac912c42b7b5d1241
Bartosz Sekuła <bartosz.sekula@episerver.com>
2019-08-01 23:38:30 +0200

    Version update script
    Use pre- builds on branches other than master

---
+++ VersionUpdate.ps1
@@ -0,0 +1,36 @@
+﻿Param([string]$branchName, [Int]$buildCounter)
+
+# Updates the AssemblyInformationalVersion property in the version.cs file based
+# on the AssemblyVersion attribute and the currently built branch
+
+if(!$branchName -or !$buildCounter) {
+    Write-Error "`$branchName and `$buildCounter parameters must be supplied"
+    exit 1
+}
+
+$assemblyVersionFile = "version.cs"
+
+$match = (Select-String -Path $assemblyVersionFile -Pattern 'AssemblyVersion[^\d]*([\d+.]+)').Matches[0]
+$assemblyVersion = $match.Groups[1].Value
+
+if (!$assemblyVersion) {
+    Write-Error "Failed to parse version information"
+    exit 1
+}
+
+switch -wildcard ($branchName) {
+    "master"   { $preReleaseInfo = "" }
+    default    { $preReleaseInfo = "-pre-{0:D6}" }
+}
+
+$informationalVersion  = "$assemblyVersion$preReleaseInfo" -f $buildCounter
+
+"AssemblyVersion: $assemblyVersion"
+"AssemblyInformationalVersion: $informationalVersion"
+
+# Publish the packageVersion parameter to team city
+"##teamcity[setParameter name='packageVersion' value='$informationalVersion']"
+
+# Replace the existing AssemblyInformationalVersion with the calculated one
+$fileContent = Get-Content $assemblyVersionFile | ForEach-Object { $_ -replace ".*AssemblyInformationalVersion.*", "" }
+Set-Content $assemblyVersionFile "$fileContent`n[assembly: AssemblyInformationalVersion(`"$informationalVersion`")]"
--- pack.ps1
+++ pack.ps1
@@ -7,17 +7,28 @@ function ZipCurrentModule
 {
     Param ([String]$moduleName)
     Robocopy.exe $defaultVersion\ $version\ /S
-    ((Get-Content -Path module.config -Raw).TrimEnd() -Replace $defaultVersion, $version ) | Set-Content -Path module.config    
+    Remove-Item "$moduleName.zip" -Force -Recurse
+    ((Get-Content -Path module.config -Raw).TrimEnd() -Replace $defaultVersion, $version ) | Set-Content -Path module.config
     Start-Process -NoNewWindow -Wait -FilePath $zip -ArgumentList "a", "$moduleName.zip", "$version", "module.config"
     ((Get-Content -Path module.config -Raw).TrimEnd() -Replace $version, $defaultVersion ) | Set-Content -Path module.config
     Remove-Item $version -Force -Recurse
 }
 
-$fullVersion=[System.Reflection.Assembly]::LoadFrom("src\alloy\bin\EPiServer.Labs.BlockEnhancements.dll").GetName().Version
-$version="$($fullVersion.major).$($fullVersion.minor).$($fullVersion.build)"
-Write-Host "Creating nuget with $version version"
+$assemblyVersionFile = "version.cs"
+
+$versionMatch = (Select-String -Path $assemblyVersionFile -Pattern 'AssemblyVersion[^\d]*([\d+.]+)').Matches[0]
+$fileVersionMatch = (Select-String -Path $assemblyVersionFile -Pattern 'AssemblyInformationalVersion[^\d]*(.+)"').Matches[0]
+$version = $versionMatch.Groups[1].Value
+$assemblyFileVersion = $fileVersionMatch.Groups[1].Value
+
+if (!$version) {
+    Write-Error "Failed to parse version information"
+    exit 1
+}
+
+Write-Host "Creating nuget with $fileVersionMatch version and $version client assets version"
 
 Set-Location src\alloy\modules\_protected\episerver-labs-block-enhancements
 ZipCurrentModule -moduleName episerver-labs-block-enhancements
 Set-Location $workingDirectory
-Start-Process -NoNewWindow -Wait -FilePath $nuget -ArgumentList "pack", "$workingDirectory\src\alloy\EPiServer.Labs.BlockEnhancements.nuspec", "-Version $version"
+Start-Process -NoNewWindow -Wait -FilePath $nuget -ArgumentList "pack", "$workingDirectory\src\alloy\EPiServer.Labs.BlockEnhancements.nuspec", "-Version $assemblyFileVersion"

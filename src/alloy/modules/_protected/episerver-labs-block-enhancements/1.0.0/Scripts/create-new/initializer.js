35194afab8aa1495c76318f4386f1c8286ea070b
Bartosz Sekuła <bartosz.sekula@episerver.com>
2019-11-01 14:01:54 +0100

    Add Inline Create feature
    User will be able to create & publish a block without the need
    to leave current content context.

---
+++ src/alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/create-new/initializer.js
@@ -0,0 +1,89 @@
+define([
+    "dojo/on",
+    "dojo/_base/declare",
+    "epi/shell/TypeDescriptorManager",
+    "epi/shell/widget/dialog/Alert",
+    "episerver-labs-block-enhancements/inline-editing/form-dialog",
+    "epi-cms/widget/ContentTypeList",
+    "epi-cms/widget/command/CreateContentFromSelector",
+    "episerver-labs-block-enhancements/create-new/create-new-block-edit-form-container",
+    "epi/i18n!epi/cms/nls/episerver.cms.components.createblock",
+    "epi/i18n!epi/cms/nls/episerver.shared.action",
+    "xstyle/css!episerver-labs-block-enhancements/create-new/styles.css"
+], function (
+    on,
+    declare,
+    TypeDescriptorManager,
+    Alert,
+    FormDialog,
+    ContentTypeList,
+    CreateContentFromSelector,
+    CreateNewBlockEditFormContainer,
+    res,
+    shared
+) {
+
+    return function() {
+        var originalSwitchView = CreateContentFromSelector.prototype._switchView;
+        CreateContentFromSelector.prototype._switchView = function(content) {
+            var autoPublish = this.autoPublish;
+            var model = this.model;
+            if (!this.createAsLocalAsset) {
+                return originalSwitchView.apply(this, arguments);
+            }
+
+            var _this = this;
+            var dialog;
+            var contentTypeList = new ContentTypeList({
+                allowedTypes: this.allowedTypes,
+                restrictedTypes: this.restrictedTypes,
+                localAsset: true,
+                parentLink: content.contentLink,
+                onContentTypeSelected: function (selectedType) {
+                    var editDialog = new FormDialog({
+                        title: shared["new"] + " " + selectedType.localizedName
+                    });
+
+                    if (dialog) {
+                        dialog.hide();
+                    }
+
+                    var form = new CreateNewBlockEditFormContainer({
+                        autoPublish: autoPublish,
+                        addToDestination: model
+                    }, editDialog.content, "last");
+                    form.reloadMetadata(content, selectedType).then(function () {
+                        editDialog.show();
+                        editDialog.togglePublishButton(false);
+                    });
+
+                    _this.own(on(editDialog, "execute", form.saveForm.bind(form)));
+                    editDialog.own(form);
+                    _this.own(editDialog);
+                }
+            });
+
+            _this.own(contentTypeList.watch("shouldSkipContentTypeSelection", function () {
+                if (contentTypeList.get("shouldSkipContentTypeSelection")) {
+                    return;
+                }
+
+                dialog = new Alert({
+                    acknowledgeActionText: epi.resources.action.close,
+                    closeIconVisible: false,
+                    content: contentTypeList,
+                    dialogClass: "epi-dialog-portrait inline-edit-dialog",
+                    title: res.title
+                });
+
+                dialog.show();
+                dialog.containerNode.classList.add("create-content-dialog");
+                _this.own(dialog);
+                contentTypeList._suggestedContentTypes.setVisibility(false);
+            }));
+            contentTypeList.set("requestedType", "episerver.core.blockdata");
+            contentTypeList.refresh();
+            _this.own(contentTypeList);
+        };
+    };
+})
--- src/alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/inline-editing/commands/update-commands.js
+++ src/alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/inline-editing/commands/update-commands.js
@@ -5,12 +5,12 @@ define([
     InlineEditCommand,
     CustomBlockEditCommand
 ) {
-    return function updateCommands(commandsOwner) {
+    return function updateCommands(commandsOwner, isInlineCreateEnabled) {
         var customBlockEditCommand = new CustomBlockEditCommand();
         commandsOwner.commands[0] = customBlockEditCommand;
         commandsOwner.own(customBlockEditCommand);
 
-        var inlineEditCommand = new InlineEditCommand();
+        var inlineEditCommand = new InlineEditCommand({isInlineCreateEnabled: isInlineCreateEnabled});
         commandsOwner.commands.splice(1, 0, inlineEditCommand);
         commandsOwner.own(inlineEditCommand);
     }

4ca43b693fbee53c8380efbaacbbfcb95da1fecb
Bartosz Sekuła <bartosz.sekula@episerver.com>
2019-11-04 23:20:37 +0100

    Add BootstrapPage and BootstrapRowBlock to test nested block scenarios

--- src/alloy/Models/Pages/StartPage.cs
+++ src/alloy/Models/Pages/StartPage.cs
@@ -16,7 +16,7 @@ namespace AlloyTemplates.Models.Pages
     [SiteImageUrl]
     [AvailableContentTypes(
         Availability.Specific,
-        Include = new[] { typeof(ContainerPage), typeof(ProductPage), typeof(StandardPage), typeof(ISearchPage), typeof(LandingPage), typeof(ContentFolder) }, // Pages we can create under the start page...
+        Include = new[] { typeof(ContainerPage), typeof(BootstrapPage), typeof(ProductPage), typeof(StandardPage), typeof(ISearchPage), typeof(LandingPage), typeof(ContentFolder) }, // Pages we can create under the start page...
         ExcludeOn = new[] { typeof(ContainerPage), typeof(ProductPage), typeof(StandardPage), typeof(ISearchPage), typeof(LandingPage) })] // ...and underneath those we can't create additional start pages
     public class StartPage : SitePageData
     {

7d58149ce118e4dba1af46e429cf8b9c95325c63
Kenneth Österberg <kenneth.osterberg@innofactor.com>
2019-11-25 11:43:14 +0200

    Add translated text to inline-edit, added embedded language file for Finnish, remove non-existing file create-new-command.js from project.

--- version.cs
+++ version.cs
@@ -1,8 +1,8 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("episerver-labs")]
 [assembly: AssemblyProduct("block-enhancements")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.5.2")]
-[assembly: AssemblyInformationalVersion("0.5.2-developerbuild")]
+[assembly: AssemblyVersion("0.5.3")]
+[assembly: AssemblyInformationalVersion("0.5.3-developerbuild")]

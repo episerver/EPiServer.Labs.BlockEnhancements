92a389e3032e9dd25c2ebd918ed998fc7525c8a2
Ben McKernan <Ben.McKernan@episerver.com>
2019-12-20 12:04:38 +0100

    Add TelemetryOptions transform
    Transform which sets whether the application is running in DXC.

---
+++ src/EPiServer.Labs.BlockEnhancements.Test/Telemetry/Internal/TelemetryOptionsTransformTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Specialized;
+using EPiServer.Labs.BlockEnhancements.Telemetry;
+using EPiServer.Labs.BlockEnhancements.Telemetry.Internal;
+using Xunit;
+
+namespace EPiServer.Labs.BlockEnhancements.Test.Telemetry.Internal
+{
+    public class TelemetryOptionsTransformTest
+    {
+        [Fact]
+        public void Transform_WhenEnvironmentName_IsProduction_ShouldSetIsDxcEnvironment_AsTrue()
+        {
+            var options = new TelemetryOptions();
+            var settings = new NameValueCollection
+            {
+                { TelemetryOptionsTransform.EnvironmentNameConfigKey, "Production" }
+            };
+
+            TelemetryOptionsTransform.Transform(options, settings);
+
+            Assert.True(options.IsDxcEnvironment);
+        }
+
+        [Fact]
+        public void Transform_WhenEnvironmentName_IsNotProduction_ShouldSetIsDxcEnvironment_AsFalse()
+        {
+            var options = new TelemetryOptions();
+            var settings = new NameValueCollection
+            {
+                { TelemetryOptionsTransform.EnvironmentNameConfigKey, "NotProduction" }
+            };
+
+            TelemetryOptionsTransform.Transform(options, settings);
+
+            Assert.False(options.IsDxcEnvironment);
+        }
+
+        [Fact]
+        public void Transform_WhenEnvironmentName_IsNull_ShouldSetIsDxcEnvironment_AsFalse()
+        {
+            var options = new TelemetryOptions();
+            var settings = new NameValueCollection();
+
+            TelemetryOptionsTransform.Transform(options, settings);
+
+            Assert.False(options.IsDxcEnvironment);
+        }
+    }
+}
---
+++ src/episerver-labs-block-enhancements/Telemetry/Internal/TelemetryInitialization.cs
@@ -0,0 +1,20 @@
+﻿using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.Initialization;
+using EPiServer.ServiceLocation;
+
+namespace EPiServer.Labs.BlockEnhancements.Telemetry.Internal
+{
+    [ModuleDependency(typeof(CmsCoreInitialization))]
+    internal class TelemetryInitialization : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            context.Services.Configure<TelemetryOptions>(TelemetryOptionsTransform.Transform);
+        }
+
+        public void Initialize(InitializationEngine context) { }
+
+        public void Uninitialize(InitializationEngine context) { }
+    }
+}

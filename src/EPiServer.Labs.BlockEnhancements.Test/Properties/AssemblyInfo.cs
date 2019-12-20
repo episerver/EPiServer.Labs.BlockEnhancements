39d828901287c2cc0d891e95313f17e8e8fda3f2
Ben McKernan <Ben.McKernan@episerver.com>
2019-12-20 11:05:52 +0100

    Add EPiServer.Labs.BlockEnhancements.Test project

---
+++ src/EPiServer.Labs.BlockEnhancements.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EPiServer.Labs.BlockEnhancements.Test")]
+[assembly: Guid("602884e4-a777-4211-88e3-8779fd83c3aa")]

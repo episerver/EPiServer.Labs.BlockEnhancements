da13b84b6737404268ee0607c99693671c03befb
Ben McKernan <Ben.McKernan@episerver.com>
2019-12-20 15:34:27 +0100

    Add IsEnabled to telemetry configuration endpoint
    Task: CMS-15111

--- src/episerver-labs-block-enhancements/Telemetry/Internal/TelemetryConfigStore.cs
+++ src/episerver-labs-block-enhancements/Telemetry/Internal/TelemetryConfigStore.cs
@@ -7,12 +7,20 @@ namespace EPiServer.Labs.BlockEnhancements.Telemetry.Internal
     [RestStore("telemetryconfig")]
     public class TelemetryConfigStore : RestControllerBase
     {
+        private readonly TelemetryOptions _options;
+
+        public TelemetryConfigStore(TelemetryOptions options)
+        {
+            _options = options;
+        }
+
         [HttpGet]
         public async Task<ActionResult> Get()
         {
             return Rest(new
             {
-                instrumentationKey = await GetInstrumentationKey()
+                instrumentationKey = await GetInstrumentationKey(),
+                isEnabled = await IsTelemetryEnabled()
             });
         }
 
@@ -21,6 +29,11 @@ namespace EPiServer.Labs.BlockEnhancements.Telemetry.Internal
             // hardcoded instrumentation key from the production subscription
             // https://portal.azure.com/#@episerver.net/resource/subscriptions/d08856e3-820a-4d39-8954-8b8916e966be/resourceGroups/cmsuitelemetryrg/providers/microsoft.insights/components/cmsui-telemetry-services-ai/overview
             return await Task.FromResult("e4b8b0bc-c592-4797-8a7e-61cf60978363");
+        }
+
+        private async Task<bool> IsTelemetryEnabled()
+        {
+            return await Task.FromResult(_options.IsTelemetryEnabled());
         }
     }
 }

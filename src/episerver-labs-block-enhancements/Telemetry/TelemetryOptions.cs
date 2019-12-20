76e0762689947cdaf683647318898a8a077c09bf
Ben McKernan <Ben.McKernan@episerver.com>
2019-12-20 10:15:36 +0100

    Add TelemetryOptions to allow opt in
    Task: CMS-15118

---
+++ src/episerver-labs-block-enhancements/Telemetry/TelemetryOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace EPiServer.Labs.BlockEnhancements.Telemetry
+{
+    public class TelemetryOptions
+    {
+        /// <summary>
+        /// Opt in to the collection of usage telemetry.
+        /// </summary>
+        /// <remarks>
+        /// Setting this to <code>false</code> does not imply opt out; usage telemetry
+        /// will always be collected in DXC environments.
+        /// </remarks>
+        public bool OptedIn { get; set; } = false;
+    }
+}

26f895562d0f66f96d9ff8cee4f596e4b3ab4178
Kenneth Österberg <kenneth.osterberg@innofactor.com>
2019-12-27 08:41:01 +0200

    Undo changes

--- version.cs
+++ version.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("episerver-labs")]
 [assembly: AssemblyProduct("block-enhancements")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.5.3")]
-[assembly: AssemblyInformationalVersion("0.5.3-developerbuild")]
+[assembly: AssemblyVersion("0.5.2")]
+[assembly: AssemblyInformationalVersion("0.5.2-developerbuild")]

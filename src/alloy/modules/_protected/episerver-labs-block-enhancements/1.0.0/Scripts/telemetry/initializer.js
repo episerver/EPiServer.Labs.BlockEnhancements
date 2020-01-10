21a88f766cf6b88ba649cb5a92815aa362293a20
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2020-01-10 10:43:45 +0100

    Move versions to the store
    Move code from ViewModel to REST store
    
    Task: CMS-15204
    Feature: CMS-14776

--- src/alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/telemetry/initializer.js
+++ src/alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/telemetry/initializer.js
@@ -2,12 +2,12 @@ define([
     "epi/dependency",
     "episerver-labs-block-enhancements/tracker",
 ], function(dependency, tracker){
-    return function(versions) {
+    return function() {
         dependency.resolve("epi.storeregistry")
             .get("episerver.labs.blockenhancements.telemetry")
             .get().then(function(telemetry){
                 if(telemetry.isEnabled){
-                    tracker.initialize(telemetry.instrumentationKey, versions);
+                    tracker.initialize(telemetry.instrumentationKey, telemetry.versions);
                     tracker.track("hello-world");
                 }
             });

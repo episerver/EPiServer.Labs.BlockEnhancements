acb8a17e4944f48ad3c8760ce939b437b8b5d7d7
Ben McKernan <Ben.McKernan@episerver.com>
2020-01-10 14:36:31 +0100

    Create endpoint to get user and client info
    The user will be identified by email, with a fallback to username. The
    client will be identified by license key, with a fallback to license
    name.
    
    Task: CMS-15112
    Feature: CMS-14776

---
+++ src/EPiServer.Labs.BlockEnhancements.Test/Telemetry/Internal/TelemetryConfigStoreTest.cs
@@ -0,0 +1,87 @@
+﻿using System.Security.Principal;
+using System.Text;
+using EPiServer.Labs.BlockEnhancements.Telemetry;
+using EPiServer.Labs.BlockEnhancements.Telemetry.Internal;
+using EPiServer.Licensing;
+using EPiServer.Security;
+using Moq;
+using Xunit;
+
+namespace EPiServer.Labs.BlockEnhancements.Test.Telemetry.Internal
+{
+    public class TelemetryConfigStoreTest
+    {
+        private readonly TelemetryOptions _telemetryOptions;
+        private readonly LicensingOptions _licensingOptions;
+        private readonly TelemetryConfigStore _telemetryConfigStore;
+
+        public TelemetryConfigStoreTest()
+        {
+            var hashHandler = new Mock<IHashHandler>();
+            hashHandler
+                .Setup(_ => _.GenerateStringHash(It.IsAny<byte[]>()))
+                .Returns((byte[] data) => "hashed-" + Encoding.Unicode.GetString(data));
+
+            var principalAccessor = Mock.Of<IPrincipalAccessor>();
+            principalAccessor.Principal = new GenericPrincipal(new GenericIdentity("username"), null);
+
+            _telemetryOptions = new TelemetryOptions();
+            _licensingOptions = new LicensingOptions();
+            _telemetryConfigStore = new TelemetryConfigStore(_telemetryOptions, _licensingOptions, principalAccessor)
+            {
+                HashHandler = hashHandler.Object
+            };
+        }
+
+        [Fact]
+        public async void GetClientHash_WhenLicenseKey_IsNotNull_ShouldSetClient_AsHashedLicenseKey()
+        {
+            _licensingOptions.LicenseKey = "key";
+            var result = await _telemetryConfigStore.Get();
+            Assert.Equal("hashed-key", result.GetData<TelemetryConfigModel>().Client);
+        }
+
+        [Fact]
+        public async void GetClientHash_WhenLicenseKey_IsNull_AndWhenLicenseFile_IsNotNull_ShouldSetClient_AsHashedLicensedCompany()
+        {
+            _licensingOptions.LicenseKey = null;
+            _telemetryConfigStore.LoadLicense = (string licenseFilePath) => new LicenseData { LicensedCompany = "company" };
+            var result = await _telemetryConfigStore.Get();
+            Assert.Equal("hashed-company", result.GetData<TelemetryConfigModel>().Client);
+        }
+
+        [Fact]
+        public async void GetClientHash_WhenLicenseKey_IsNull_AndWhenLicenseFile_IsNull_ShouldSetClient_AsNull()
+        {
+            _licensingOptions.LicenseKey = null;
+            _telemetryConfigStore.LoadLicense = (string licenseFilePath) => null;
+            var result = await _telemetryConfigStore.Get();
+            Assert.Null(result.GetData<TelemetryConfigModel>().Client);
+        }
+
+        [Fact]
+        public async void GetClientHash_WhenLicenseKey_IsNull_AndLoadLicense_ThrowsLicenseException_ShouldSetClient_AsNull()
+        {
+            _licensingOptions.LicenseKey = null;
+            _telemetryConfigStore.LoadLicense = (string licenseFilePath) => throw new LicenseException();
+            var result = await _telemetryConfigStore.Get();
+            Assert.Null(result.GetData<TelemetryConfigModel>().Client);
+        }
+
+        [Fact]
+        public async void GetUserHash_WhenProfileEmail_IsNull_ShouldSetUser_AsHashedUsername()
+        {
+            _telemetryConfigStore.LoadEmailFromProfile = (string username) => null;
+            var result = await _telemetryConfigStore.Get();
+            Assert.Equal("hashed-username", result.GetData<TelemetryConfigModel>().User);
+        }
+
+        [Fact]
+        public async void GetUserHash_WhenProfileEmail_IsNotNull_ShouldSetUser_AsHashedEmail()
+        {
+            _telemetryConfigStore.LoadEmailFromProfile = (string username) => "user@domain.com";
+            var result = await _telemetryConfigStore.Get();
+            Assert.Equal("hashed-user@domain.com", result.GetData<TelemetryConfigModel>().User);
+        }
+    }
+}
---
+++ src/episerver-labs-block-enhancements/Telemetry/Internal/TelemetryConfigModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace EPiServer.Labs.BlockEnhancements.Telemetry.Internal
+{
+    internal class TelemetryConfigModel
+    {
+        public string Client { get; internal set; }
+
+        public string InstrumentationKey { get; internal set; }
+
+        public bool IsEnabled { get; internal set; }
+
+        public string User { get; internal set; }
+
+        public IDictionary<string, string> Versions { get; internal set; }
+    }
+}

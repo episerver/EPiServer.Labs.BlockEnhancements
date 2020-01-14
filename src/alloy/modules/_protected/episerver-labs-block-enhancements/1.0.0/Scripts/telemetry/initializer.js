843290ec2b835f570b2090b224b52f3e1441c560
Ben McKernan <Ben.McKernan@episerver.com>
2020-01-14 15:08:42 +0100

    Set the authenticated user context for AppInsights
    Task: CMS-15222

--- src/alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/telemetry/initializer.js
+++ src/alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/telemetry/initializer.js
@@ -1,13 +1,13 @@
 define([
     "epi/dependency",
     "episerver-labs-block-enhancements/tracker",
-], function(dependency, tracker){
-    return function() {
+], function (dependency, tracker) {
+    return function () {
         dependency.resolve("epi.storeregistry")
             .get("episerver.labs.blockenhancements.telemetry")
-            .get().then(function(telemetry){
-            tracker.initialize(telemetry.isEnabled, telemetry.instrumentationKey, telemetry.versions);
-            tracker.track("hello-world");
-        });
+            .get().then(function (telemetry) {
+                tracker.initialize(telemetry.isEnabled, telemetry.instrumentationKey, telemetry.versions, telemetry.user, telemetry.client);
+                tracker.track("hello-world");
+            });
     }
 });

561882994df48e88ebf89d3c82703b1ad1bf56cf
Nicklas Israelsson <nicklas.israelsson@episerver.com>
2020-01-22 13:59:13 +0100

    Track publish command
    Adding tracking code to the lowest common denominator for all commands
    used in the labs code: `_ChangeContentStatus`
    
    Commands inheriting from that can use the `trackCommand` flag to enable
    tracking.
    
    Enabling `trackCommand` on the `PublishCommand` will start tracking
    almost all labs commands since most of them inherit from that but for
    now all of them will use the "default" `commandType`.
    
    KQL query to get data:
    
    ```
    customEvents
    | where name == "publish"
    | sort by timestamp desc
    | project CommandType = customDimensions["command-type"],
      ContentType = customDimensions["content-type"]
    | where CommandType != "" and ContentType != ""
    ```
    
    CMS-15251

---
+++ src/alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/telemetry/patch-cms-commands.js
@@ -0,0 +1,41 @@
+define([
+    "dojo/when",
+    "epi-cms/contentediting/command/_ChangeContentStatus",
+    "epi-cms/contentediting/command/Publish",
+    "episerver-labs-block-enhancements/tracker",
+], function (
+    when,
+    _ChangeContentStatus,
+    PublishCommand,
+    tracker) {
+    return function () {
+        PublishCommand.prototype.commandType = "default";
+        PublishCommand.prototype.trackCommand = true;
+
+        var originalExecute = _ChangeContentStatus.prototype.execute;
+        _ChangeContentStatus.prototype.execute = function () {
+            if (!this.trackCommand){
+                return originalExecute.apply(this, arguments);
+            }
+
+            return when(originalExecute.apply(this, arguments)).then(function () {
+                var isPage = this.model.contentData.capabilities.isPage;
+                var isBlock = this.model.contentData.capabilities.isBlock;
+
+                // dont track if it's not a block or a page
+                if (!isPage && !isBlock){
+                    return;
+                }
+
+                var additionalData = this._getTrackingData && this._getTrackingData();
+
+                tracker.track("publish", Object.assign({
+                    "command-type": this.commandType,
+                    "content-type": isPage ? "page" : "block"
+                }, additionalData));
+            }.bind(this));
+        }
+
+        _ChangeContentStatus.prototype.execute.nom = "execute"
+    }
+});

6271bb26dd233fa6f6bae416bbd62b3531234662
Ben McKernan <Ben.McKernan@episerver.com>
2020-01-22 11:32:05 +0100

    Strip console logs in production build

--- src/episerver-cms-telemetry/webpack.config.js
+++ src/episerver-cms-telemetry/webpack.config.js
@@ -1,17 +1,30 @@
 const path = require("path");
 
 module.exports = (env, argv) => {
+    const isProduction = argv.mode === "production";
     return {
-        devtool: argv.mode === "production" ? "source-map" : "inline-source-map",
-        resolve: {
-            extensions: [".js"]
-        },
+        devtool: isProduction ? "source-map" : "inline-source-map",
         entry: "./src/tracker.js",
         output: {
             filename: "tracker.js",
             libraryTarget: "amd",
             libraryExport: "default",
             path: path.resolve(__dirname, "./../alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts")
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"],
+                            plugins: isProduction ? ["babel-plugin-transform-remove-console"] : []
+                        }
+                    }
+                }
+            ]
         }
     }
 }

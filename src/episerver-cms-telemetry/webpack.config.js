689dcaa2cda34daa080b4595b4d2662139658035
Ben McKernan <Ben.McKernan@episerver.com>
2020-01-23 10:20:49 +0100

    Build production and debug versions of tracker

--- src/episerver-cms-telemetry/webpack.config.js
+++ src/episerver-cms-telemetry/webpack.config.js
@@ -1,30 +1,57 @@
 const path = require("path");
 
-module.exports = (env, argv) => {
-    const isProduction = argv.mode === "production";
-    return {
-        devtool: isProduction ? "source-map" : "inline-source-map",
-        entry: "./src/tracker.js",
-        output: {
-            filename: "tracker.js",
-            libraryTarget: "amd",
-            libraryExport: "default",
-            path: path.resolve(__dirname, "./../alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts")
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: {
-                        loader: "babel-loader",
-                        options: {
-                            presets: ["@babel/preset-env"],
-                            plugins: isProduction ? ["babel-plugin-transform-remove-console"] : []
+module.exports = () => {
+    return [
+        {
+            devtool: "source-map",
+            mode: "production",
+            entry: "./src/tracker.js",
+            output: {
+                filename: "tracker.js",
+                libraryTarget: "amd",
+                libraryExport: "default",
+                path: path.resolve(__dirname, "./../alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts")
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: "babel-loader",
+                            options: {
+                                presets: ["@babel/preset-env"],
+                                plugins: ["babel-plugin-transform-remove-console"]
+                            }
                         }
                     }
-                }
-            ]
+                ]
+            }
+        },
+        {
+            devtool: "inline-source-map",
+            mode: "development",
+            entry: "./src/tracker.js",
+            output: {
+                filename: "tracker.js.uncompressed.js",
+                libraryTarget: "amd",
+                libraryExport: "default",
+                path: path.resolve(__dirname, "./../alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts")
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        use: {
+                            loader: "babel-loader",
+                            options: {
+                                presets: ["@babel/preset-env"]
+                            }
+                        }
+                    }
+                ]
+            }
         }
-    }
+    ]
 }

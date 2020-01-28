4e2e4f4440baa6530cce82efd7b94a25eb8b1d67
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2020-01-28 13:19:43 +0100

    Add custom validator that stop Publish action
    There is no easy way to interrupt the `Publish` action, so
    to simplify tracking failed scenarios we will now cancel this
    action whenever a content item's name is `DoNotPublish` which
    effectively makes the `Publish` action to fail.
    
    Task: CMS-15295

---
+++ src/alloy/Business/Initialization/PublishValidation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Shell.UI.Internal;
+
+namespace AlloyTemplates.Business.Initialization
+{
+    [ServiceConfiguration(typeof(IEventListener), Lifecycle = ServiceInstanceScope.Singleton)]
+    public class PublishValidation: IEventListener
+    {
+        private readonly IContentEvents _contentEvents;
+
+        public PublishValidation(IContentEvents contentEvents)
+        {
+            _contentEvents = contentEvents;
+        }
+
+        public void Start()
+        {
+            _contentEvents.PublishingContent += _contentEvents_PublishingContent;
+        }
+
+        public void Stop()
+        {
+            _contentEvents.PublishingContent -= _contentEvents_PublishingContent;
+        }
+
+        private void _contentEvents_PublishingContent(object sender, EPiServer.ContentEventArgs e)
+        {
+            if (string.Compare(e.Content.Name, "DoNotPublish", StringComparison.CurrentCultureIgnoreCase) == 0)
+            {
+                e.CancelAction = true;
+                e.CancelReason = "Can't publish";
+            }
+        }
+    }
+}

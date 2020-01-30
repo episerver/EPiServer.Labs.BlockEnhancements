519a036d62665de9a338df9e9ef1967703537380
Ben McKernan <Ben.McKernan@episerver.com>
2020-01-30 08:06:20 +0100

    Support running tests from CLI

--- test.cmd
+++ test.cmd
@@ -7,6 +7,10 @@ REM Set Release or Debug configuration.
 IF "%1"=="Release" (set CONFIGURATION=Release) ELSE (set CONFIGURATION=Debug)
 ECHO Testing in %CONFIGURATION%
 
+ECHO Running c# tests
+CALL dotnet test src/EPiServer.Labs.BlockEnhancements.Test/EPiServer.Labs.BlockEnhancements.Test.csproj --no-build --verbosity normal
+IF %errorlevel% NEQ 0 EXIT /B %errorlevel%
+
 REM Run karma tests
 CALL yarn karma
 IF %errorlevel% NEQ 0 EXIT /B %errorlevel%

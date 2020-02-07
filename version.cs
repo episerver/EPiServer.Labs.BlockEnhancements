fbe1f954b68057e4ed6da610fd95262896a340cb
Nicklas Israelsson <nicklas.israelsson@episerver.com>
2020-02-07 08:57:30 +0100

    Update version to 0.7.0

--- version.cs
+++ version.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("episerver-labs")]
 [assembly: AssemblyProduct("block-enhancements")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.6.3")]
-[assembly: AssemblyInformationalVersion("0.6.3-developerbuild")]
+[assembly: AssemblyVersion("0.7.0")]
+[assembly: AssemblyInformationalVersion("0.7.0-developerbuild")]

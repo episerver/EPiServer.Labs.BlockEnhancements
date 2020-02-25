445d91b670b6036d3c194d90f51f3316483e9f3c
Nicklas Israelsson <nicklas.israelsson@episerver.com>
2020-02-25 10:25:29 +0100

    Updating version to 0.7.1
    Fixes #48 and CMS-15457

--- version.cs
+++ version.cs
@@ -1,8 +1,8 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("episerver-labs")]
 [assembly: AssemblyProduct("block-enhancements")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.7.0")]
-[assembly: AssemblyInformationalVersion("0.7.0-developerbuild")]
+[assembly: AssemblyVersion("0.7.1")]
+[assembly: AssemblyInformationalVersion("0.7.1-developerbuild")]

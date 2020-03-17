740444259d15934879d6666d529a5fbbb1207ea8
Bartosz Sekuła <bartosz.sekula@episerver.com>
2020-03-17 11:48:49 +0100

    v0.7.2

--- version.cs
+++ version.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("episerver-labs")]
 [assembly: AssemblyProduct("block-enhancements")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.7.1")]
-[assembly: AssemblyInformationalVersion("0.7.1-developerbuild")]
+[assembly: AssemblyVersion("0.7.2")]
+[assembly: AssemblyInformationalVersion("0.7.2-developerbuild")]

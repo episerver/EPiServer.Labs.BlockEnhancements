498b03ba911c38b2f23fb73525113e8da20a2421
Bartosz Sekuła <bartosz.sekula@episerver.com>
2020-05-04 08:18:59 +0200

    Remove unused files
    Those files were moved to episerver-telemetry-ui package

--- src/Episerver.Telemetry.UI.Tests/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Episerver.Telemetry.UI.Tests")]
-[assembly: Guid("602884e4-a777-4211-88e3-8779fd83c3aa")]
--- src/Episerver.Telemetry.UI.Tests/RestResultExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿using EPiServer.Shell.Services.Rest;
-
-namespace Episerver.Telemetry.UI.Tests
-{
-    public static class RestResultExtensions
-    {
-        public static T GetData<T>(this RestResult result) => (T)result.Data;
-    }
-}

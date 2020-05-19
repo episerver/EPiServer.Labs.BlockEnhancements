549994a154da6f04ec5e5e1d8e7d5a81a0242759
Grzegorz Wiechec <grzegorz.wiechec@episerver.com>
2020-05-19 12:25:10 +0200

    Move Blocks Enhancements tracking
    Features tracking should be done in Blocks Enhancements project
    instead of telemetry project.
    
    Feature: CMS-15772
    Task: CMS-15866

--- src/alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/telemetry/initializer.js
+++ src/alloy/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/telemetry/initializer.js
@@ -1,7 +1,14 @@
 define([
+    "episerver-telemetry-ui/tracker-factory",
     "episerver-labs-block-enhancements/telemetry/patch-cms-commands"
-], function (patchCmsCommands) {
-    return function () {
+], function (
+    trackerFactory,
+    patchCmsCommands
+) {
+    return function (options) {
         patchCmsCommands();
-    }
+
+        var tracker = trackerFactory.getTracker("cms");
+        tracker.track("featureOptions", options);
+    };
 });

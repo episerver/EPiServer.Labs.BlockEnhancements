2f20aa3786bdd0418ae72737db6de9b5dc681723
Bartosz Sekuła <bartosz.sekula@episerver.com>
2020-05-28 15:14:06 +0200

    Remove the custom label for built-in Edit command
    Users find it confusing that the built-in Edit command
    is now displayed as 'View'.
    This commit reverts that change and moves the new
    Block Inline Edit command to a separate category
    below the built-in Edit.
    
    Closes #54

--- src/AlloyMvcTemplates/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/inline-editing/commands/update-commands.js
+++ src/AlloyMvcTemplates/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/inline-editing/commands/update-commands.js
@@ -1,15 +1,9 @@
 define([
-    "episerver-labs-block-enhancements/inline-editing/commands/block-menu-inline-edit",
-    "episerver-labs-block-enhancements/inline-editing/commands/custom-block-edit"
+    "episerver-labs-block-enhancements/inline-editing/commands/block-menu-inline-edit"
 ], function (
-    InlineEditCommand,
-    CustomBlockEditCommand
+    InlineEditCommand
 ) {
     return function updateCommands(commandsOwner, isInlineCreateEnabled) {
-        var customBlockEditCommand = new CustomBlockEditCommand();
-        commandsOwner.commands[0] = customBlockEditCommand;
-        commandsOwner.own(customBlockEditCommand);
-
         var inlineEditCommand = new InlineEditCommand({isInlineCreateEnabled: isInlineCreateEnabled});
         commandsOwner.commands.splice(1, 0, inlineEditCommand);
         commandsOwner.own(inlineEditCommand);

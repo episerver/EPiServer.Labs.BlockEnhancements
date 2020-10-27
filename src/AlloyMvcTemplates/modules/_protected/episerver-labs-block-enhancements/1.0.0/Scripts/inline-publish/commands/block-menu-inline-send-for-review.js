975f859bd9727496352390a104fe4601d409c7ff
Linh Nguyen <Linh.Nguyen@episerver.com>
2020-10-27 19:55:25 +0100

    Remove block-menu-inline-command-mixin
    Task: CMS-17170
    Feature: CMS-16632

--- src/AlloyMvcTemplates/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/inline-publish/commands/block-menu-inline-send-for-review.js
+++ src/AlloyMvcTemplates/modules/_protected/episerver-labs-block-enhancements/1.0.0/Scripts/inline-publish/commands/block-menu-inline-send-for-review.js
@@ -1,12 +1,29 @@
 define([
     "dojo/_base/declare",
-    "episerver-labs-block-enhancements/inline-editing/commands/inline-send-for-review",
-    "episerver-labs-block-enhancements/inline-editing/commands/block-menu-inline-command-mixin"
+    "episerver-labs-block-enhancements/inline-editing/commands/inline-send-for-review"
 ], function (
     declare,
     InlineSendForReview,
     _InlineCommandMixin
 ) {
-    return declare([InlineSendForReview, _InlineCommandMixin]);
+    return declare([InlineSendForReview], {
+        _onModelChange: function () {
+            if (this.model instanceof Array) {
+                //this command should be available only if one item selected
+                if (this.model.length === 1) {
+                    this.model = this.model[0];
+                } else {
+                    this.model = null;
+                }
+            }
+
+            if (!this.model) {
+                this.set("canExecute", false);
+                return;
+            }
+
+            this.inherited(arguments);
+        }
+    });
 });
 

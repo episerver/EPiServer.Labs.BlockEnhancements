e5fc814c48387d20f70ea223422bdc96edbcf23e
Bartosz Sekuła <bartosz.sekula@episerver.com>
2021-10-01 15:34:43 +0200

    Filter out local items after publish
    CMS-20300

---
+++ src/EPiServer.Labs.BlockEnhancements/InlineBlocksEditing/CustomProjectService.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using EPiServer.Cms.Shell.Service.Internal;
+using EPiServer.Cms.Shell.UI.Rest;
+using EPiServer.Cms.Shell.UI.Rest.Approvals;
+using EPiServer.Cms.Shell.UI.Rest.Projects;
+using EPiServer.Cms.Shell.UI.Rest.Projects.Internal;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.Framework.Configuration;
+using EPiServer.Framework.Localization;
+using EPiServer.Shell.Services.Rest;
+
+namespace EPiServer.Labs.BlockEnhancements.InlineBlocksEditing
+{
+    public class CustomProjectService : ProjectService
+    {
+        private readonly ProjectService _defaultProjectService;
+        private readonly LocalBlockResolver _localBlockResolver;
+
+        public CustomProjectService(ProjectService defaultProjectService, ProjectRepository projectRepository,
+            ProjectPublisher projectPublisher, ContentService contentService,
+            IContentChangeManager contentChangeManager,
+            LanguageSelectorFactory languageSelectorFactory, CurrentProject currentProject,
+            ISiteConfigurationRepository siteConfigurationRepository, IConfigurationSource configurationSource,
+            ApprovalService approvalService, LocalizationService localizationService,
+            LocalBlockResolver localBlockResolver) : base(projectRepository,
+            projectPublisher, contentService, contentChangeManager, languageSelectorFactory, currentProject,
+            siteConfigurationRepository, configurationSource, approvalService, localizationService)
+        {
+            _defaultProjectService = defaultProjectService;
+            _localBlockResolver = localBlockResolver;
+        }
+
+        public override RangedItems<ProjectItem> GetItems(int projectId, int? start, int? end)
+        {
+            var rangedItems = _defaultProjectService.GetItems(projectId, start, end);
+            return EnsureLocalOnly(rangedItems);
+        }
+
+        public override RangedItems<ProjectItem> GetItems(IEnumerable<ContentReference> contentReferences)
+        {
+            var rangedItems = _defaultProjectService.GetItems(contentReferences);
+            return EnsureLocalOnly(rangedItems);
+        }
+
+        private RangedItems<ProjectItem> EnsureLocalOnly(RangedItems<ProjectItem> projectItems)
+        {
+            if (!_localBlockResolver.ShouldFilterOutLocalBlocks())
+                return projectItems;
+
+            var filtered = projectItems.Items.Where(x =>
+                !_localBlockResolver.IsLocal(x.ContentLink.ToReferenceWithoutVersion()));
+            return new RangedItems<ProjectItem>(filtered, new ItemRange
+            {
+                Total = filtered.Count(),
+                Start = 0,
+                End = filtered.Count() - 1
+            });
+        }
+    }
+}
--- src/EPiServer.Labs.BlockEnhancements/InlineBlocksEditing/ProjectItemStoreHiderInitializer.cs
+++ src/EPiServer.Labs.BlockEnhancements/InlineBlocksEditing/ProjectItemStoreHiderInitializer.cs
@@ -1,7 +1,14 @@
+using EPiServer.Cms.Shell.Service.Internal;
+using EPiServer.Cms.Shell.UI.Rest;
+using EPiServer.Cms.Shell.UI.Rest.Approvals;
+using EPiServer.Cms.Shell.UI.Rest.Projects;
+using EPiServer.Cms.Shell.UI.Rest.Projects.Internal;
+using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.Framework;
-using EPiServer.Framework.Cache;
+using EPiServer.Framework.Configuration;
 using EPiServer.Framework.Initialization;
+using EPiServer.Framework.Localization;
 using EPiServer.ServiceLocation;
 
 namespace EPiServer.Labs.BlockEnhancements.InlineBlocksEditing
@@ -11,11 +18,28 @@ namespace EPiServer.Labs.BlockEnhancements.InlineBlocksEditing
     {
         public void ConfigureContainer(ServiceConfigurationContext context)
         {
-            context.Services.Intercept<ProjectRepository>(
-                (locator, defaultProjectRepository) => new CustomProjectRepository(defaultProjectRepository,
-                    ServiceLocator.Current.GetInstance<BlockEnhancementsOptions>(),
-                    ServiceLocator.Current.GetInstance<IContentLoader>(),
-                    ServiceLocator.Current.GetInstance<IObjectInstanceCache>()));
+            context.Services.Intercept<ProjectLoaderService>(
+                (locator, defaultProjectLoaderService) => new CustomProjectLoaderService(defaultProjectLoaderService,
+                    ServiceLocator.Current.GetInstance<ProjectRepository>(),
+                    ServiceLocator.Current.GetInstance<CurrentProject>(),
+                    ServiceLocator.Current.GetInstance<ContentLoaderService>(),
+                    ServiceLocator.Current.GetInstance<ISiteConfigurationRepository>(),
+                    ServiceLocator.Current.GetInstance<IConfigurationSource>(),
+                    ServiceLocator.Current.GetInstance<LocalBlockResolver>()));
+
+            context.Services.Intercept<ProjectService>(
+                (locator, defaultProjectService) => new CustomProjectService(defaultProjectService,
+                    ServiceLocator.Current.GetInstance<ProjectRepository>(),
+                    ServiceLocator.Current.GetInstance<ProjectPublisher>(),
+                    ServiceLocator.Current.GetInstance<ContentService>(),
+                    ServiceLocator.Current.GetInstance<ContentChangeManager>(),
+                    ServiceLocator.Current.GetInstance<LanguageSelectorFactory>(),
+                    ServiceLocator.Current.GetInstance<CurrentProject>(),
+                    ServiceLocator.Current.GetInstance<ISiteConfigurationRepository>(),
+                    ServiceLocator.Current.GetInstance<IConfigurationSource>(),
+                    ServiceLocator.Current.GetInstance<ApprovalService>(),
+                    ServiceLocator.Current.GetInstance<LocalizationService>(),
+                    ServiceLocator.Current.GetInstance<LocalBlockResolver>()));
         }
 
         public void Initialize(InitializationEngine context)

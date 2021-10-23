d71fe7efe804efb47dc10d182ed76600e4085492
Bartosz Sekuła <bartosz.sekula@episerver.com>
2021-10-23 20:17:21 +0200

    Remove unused commands from pack.ps1

--- pack.ps1
+++ pack.ps1
@@ -3,19 +3,8 @@ $workingDirectory = Get-Location
 $zip = "$workingDirectory\packages\7-Zip.CommandLine.18.1.0\tools\7za.exe"
 $nuget = "$workingDirectory\build\tools\nuget.exe"
 
-function ZipCurrentModule
-{
-    Param ([String]$moduleName)
-    Robocopy.exe $defaultVersion\ $version\ /S
-    Remove-Item "$moduleName.zip" -Force -Recurse -ErrorAction Ignore
-    ((Get-Content -Path module.config -Raw).TrimEnd() -Replace $defaultVersion, $version ) | Set-Content -Path module.config
-    Start-Process -NoNewWindow -Wait -FilePath $zip -ArgumentList "a", "$moduleName.zip", "$version", "module.config"
-    ((Get-Content -Path module.config -Raw).TrimEnd() -Replace $version, $defaultVersion ) | Set-Content -Path module.config
-    Remove-Item $version -Force -Recurse
-}
-
+#read version numbers from version.cs
 $assemblyVersionFile = "version.cs"
-
 $versionMatch = (Select-String -Path $assemblyVersionFile -Pattern 'AssemblyVersion[^\d]*([\d+.]+)').Matches[0]
 $fileVersionMatch = (Select-String -Path $assemblyVersionFile -Pattern 'AssemblyInformationalVersion[^\d]*(.+)"').Matches[0]
 $version = $versionMatch.Groups[1].Value

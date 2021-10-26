5e06af33e74c07a874a349ff713948ad274684a9
Bartosz Sekuła <bartosz.sekula@episerver.com>
2021-10-26 15:25:16 +0200

    Version 0.11.1

--- version.cs
+++ version.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("episerver-labs")]
 [assembly: AssemblyProduct("block-enhancements")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.11.0")]
-[assembly: AssemblyInformationalVersion("0.11.0-developerbuild")]
+[assembly: AssemblyVersion("0.11.1")]
+[assembly: AssemblyInformationalVersion("0.11.1-developerbuild")]
